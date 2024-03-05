c7387eb8d6bb526211db892b058cac670d3fcee1
Big_A <97909214+ArianAghamohseni@users.noreply.github.com>
2024-03-05 21:48:57 +0330

    Add files via upload

---
+++ Parameter Recovery/GetParametersRange.py
@@ -0,0 +1,32 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(os.getcwd())))
+
+import numpy as np
+import pandas as pd
+import cmdstanpy
+from utils.utils import get_parameters_range
+
+# Loading words and non-words with zipf and predicted probabilities
+root = "../"
+behavioural_data_root = root +  "Datasets/behavioral_data/selected_data/"
+dataset_path = root + "Datasets/AI Models Results/fastText_FC.csv"
+path_to_stan_output = root + "Estimations/Results/hierarchical/stan_results/ANN-RDM_s_am_FT"
+
+
+word_nword_df = pd.read_csv(dataset_path, header=None,
+                            names =["string", "freq",  "label", "zipf",
+                                    "category", "word_prob", "non_word_prob"])
+
+# Reading LDT Data
+behavioural_df = pd.read_csv(behavioural_data_root + "LDT_data.csv",
+                             header=None,
+                             names=["accuracy", "rt", "string", "response",
+                                    "participant", "minRT", "participant_id"])
+# Merging  behavioral dataframe with word_nonword_df to have words and non-words data with behavioral data
+behavioural_df = pd.merge(behavioural_df, word_nword_df, on="string", how="left").dropna().reset_index(drop=True)
+behavioural_df = behavioural_df.drop(["freq", "participant"], axis=1)
+
+ranges = get_parameters_range(path_to_stan_output, behavioural_df)
+ranges.index = ["alpha", "b", "k_1", "k_2", "threshold_word", "threshold_nonword", "ndt"]
+ranges.to_csv("Data/params_range.csv")
